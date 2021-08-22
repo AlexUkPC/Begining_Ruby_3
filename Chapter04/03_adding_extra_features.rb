6c07e5da3d3a5a722ddd35a9487a8f4fa777ee86
AlexUkPC <alexrogna@yahoo.com>
2021-08-22 13:30:31 +0100

    Chapter 4

---
+++ Chapter04/03_adding_extra_features.rb
@@ -0,0 +1,13 @@
+lines = File.readlines("text.txt")
+line_count = lines.size
+text = lines.join
+word_count = text.split.length
+character_count = text.length
+character_count_nospaces = text.gsub(/\s+/, '').length
+paragraph_count = text.split(/\n\n/).length
+sentence_count = text.split(/\.|\?|!/).length
+stopwords = %w{the a by on for of are with just but and to the my in I has some}
+all_words = text.scan(/\w+/)
+good_words = all_words.reject{ |word| stopwords.include?(word) }
+good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).to_i
+puts "#{good_percentage}% of words are non-fluff words"
